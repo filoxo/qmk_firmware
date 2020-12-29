77a54c8292d3eb6874360342cc8ab4537fac24fa
Carlos Filoteo <c.filoteo12@gmail.com>
2020-12-28 21:20:17 -0700

    Update

--- keyboards/keebio/quefrency/keymaps/wasatchchief/keymap.c
+++ keyboards/keebio/quefrency/keymaps/wasatchchief/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_VOLD, KC_VOLU, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     KC_MUTE, KC_F1,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, 
     KC_F2,   KC_F3,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_UP,  FN, 
-    KC_F4,   KC_F5,   KC_LCTL, KC_LGUI, KC_LALT, FN,      KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, GUI_L,   KC_DOWN, CTL_R
+    KC_F4,   KC_F5,   KC_LCTL, KC_LGUI, KC_LALT, FN,      KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, GUI_L, GUI_L,   KC_DOWN, CTL_R
   ),
 
   [_FN] = LAYOUT_60_with_macro(
